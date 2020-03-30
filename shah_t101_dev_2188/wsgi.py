86daccceea483eb557dd9d522695fc943236dc4b
Crowdbotics <team@crowdbotics.com>
2020-03-30 09:12:12 +0000

    Update models

--- shah_t101_dev_2188/wsgi.py
+++ shah_t101_dev_2188/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shah_t101_dev_2188.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shah_t101_dev_2188.settings")
 
 application = get_wsgi_application()
